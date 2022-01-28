c4ee9bec47cb1aa65ce0a56b643d8e09e6563e67
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-01-28 12:10:04 +0300

    Второе задание

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = "Калькулятор вёрстки",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 129,
+    rollback = 55,
+    fullPrice = 500,
+    adaptive = true;
 alert('Первое задание по JS!');
-console.log('Hello world!');    
+console.log('Hello world!');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback / 100));
