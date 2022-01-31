60bbf2f9f4e4369b6d9ba9af61b208fb4563c0cd
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-01-31 19:59:00 +0300

    add(script.js):solution 3 tasks

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+"use strict";
 let title = "Калькулятор вёрстки",
     screens = "Простые, Сложные, Интерактивные",
-    screenPrice = 129,
+    screenPrice = 18000,
     rollback = 55,
-    fullPrice = 500,
+    fullPrice = 30000,
     adaptive = true;
 alert('Первое задание по JS!');
 console.log('Hello world!');
@@ -11,7 +12,32 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " долларов");
-console.log("Стоимость разработки сайта " + fullPrice + " долларов");
-console.log(screens.toLowerCase().split(","));
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback / 100));
+
+title = prompt('Как называется наш проект?');
+screens = prompt('Какие типы экранов нужно разработать?', 'Простые');
+screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice1 = +prompt('Сколько это будет стоить?'),
+    service2 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice2 = +prompt('Сколько это будет стоить?');
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - 2000);
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+    console.log("Даём скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log("Даём скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+    console.log("Что то пошло не так");
+}
